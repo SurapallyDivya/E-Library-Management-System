d5ea168767fa2b87b1cb1777a1e3ed96e21fae98
SurapallyDivya <divya.surapally2@gmail.com>
2023-04-07 18:48:13 +0530

    all files added

---
+++ src/com/e-lib/servlets/StudentLogin.java
@@ -0,0 +1,78 @@
+package com.javatpoint.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.javatpoint.dao.LibrarianDao;
+import com.javatpoint.dao.StudentDao;
+
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class StudentLogin
+ */
+@WebServlet("/StudentLogin")
+public class StudentLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public StudentLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Student Section</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		String sid=request.getParameter("studentId");
+		String password=request.getParameter("password");
+		if(StudentDao.authenticate(sid, password)){
+			HttpSession session=request.getSession();
+			session.setAttribute("studentId",sid);
+			
+			request.getRequestDispatcher("navlibrarian.html").include(request, response);
+			request.getRequestDispatcher("Student.html").include(request, response);
+			
+		}else{
+			request.getRequestDispatcher("navhome.html").include(request, response);
+			out.println("<div class='container'>");
+			out.println("<h3>Username or password error</h3>");
+			request.getRequestDispatcher("Student.html").include(request, response);
+			out.println("</div>");
+		}
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
+	}
+
+}
